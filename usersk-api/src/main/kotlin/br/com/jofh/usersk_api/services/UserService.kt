78f7cc6df90c31804255558eeca8f61463c31d06
Orlando Ferreira <orlandofnfilho@gmail.com>
2025-02-13 06:23:04 -0300

    Adiciona busca por cpfCnpj

--- usersk-api/src/main/kotlin/br/com/jofh/usersk_api/services/UserService.kt
+++ usersk-api/src/main/kotlin/br/com/jofh/usersk_api/services/UserService.kt
@@ -1,10 +1,9 @@
 package br.com.jofh.usersk_api.services
 
 import br.com.jofh.usersk_api.entities.User
-import java.util.Optional
 
 interface UserService {
 
     fun saveUser(user: User): User
-    fun findUserByCpfCnpj(cpfCnpj: String): Optional<User>
+    fun findUserByCpfCnpj(cpfCnpj: String): User
 }
